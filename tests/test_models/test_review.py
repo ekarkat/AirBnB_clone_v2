541526e07f95afdad801b5bb83df78a272e39589
ekarkat <ekarkat@gmail.com>
2023-11-23 01:56:28 +0000

    fix unitest

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -22,14 +22,19 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
+        pl = Place()
+        new.place_id = pl.id
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
+        us = User()
+        new.user_id = us.id
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
         new = self.value()
+        new.text = "good"
         self.assertEqual(type(new.text), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = "John"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = "Doe"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        new.email = "nE6rP@example.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = "123456"
         self.assertEqual(type(new.password), str)
