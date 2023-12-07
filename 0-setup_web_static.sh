4f0cb588e6e49967d66086d579de4653fa33f06f
ekarkat <ekarkat@gmail.com>
2023-12-07 22:18:09 +0000

    ga0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,48 +1,38 @@
 #!/usr/bin/env bash
-#setting up nginx
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx
 apt-get update
 apt-get install -y nginx
 
-# Create web_static folder if it doesnt exist and shared
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-# Granting permission to ubuntu user for /data
-sudo chown -R "ubuntu":"ubuntu" /data/
-
-# Create a fake HTML file 
-echo "Hello hbnb_static!" > /data/web_static/releases/test/index.html
-
-# Create a symbolik link 
+echo "hola hbtn" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Configuring nginx 
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
 echo "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-	root /var/www/html;
-	index index.html;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-	add_header X-Served-By $(hostname) always;
-
-	location /hbnb_static {
-		alias /data/web_static/current/;
-		index index.html;
-	}
-
-	location /redirect_me {
-		return 301 http://justdont.tech;
-	}
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
 
     error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/html;
-        internal;
+    location /404 {
+      root /var/www/html;
+      internal;
     }
 }" > /etc/nginx/sites-available/default
 
-# Restart nginx services 
-sudo service nginx restart
+service nginx restart
