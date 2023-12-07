ba146cb38ca4558ca0102e213e017ee0dedb530f
ekarkat <ekarkat@gmail.com>
2023-12-07 21:11:57 +0000

    tas0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#setting up nginx
+
+# Install Nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Getting permission to edit config files:
+sudo chown -R "$USER":"$USER" /etc/nginx/sites-enabled
+
+# Create web_static folder if it doesnt exist and shared
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+# Granting permission to ubuntu user for /data
+sudo chown -R "ubuntu":"ubuntu" /data/
+
+# Create a fake HTML file 
+echo "Hello World!" > /data/web_static/releases/test/index.html
+
+# Create a symbolik link 
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Configuring nginx 
+sudo rm /etc/nginx/sites-enabled/default
+echo "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.html;
+
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+
+	location /redirect_me {
+		return 301 http://justdont.tech;
+	}
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/html;
+        internal;
+    }
+}" > /etc/nginx/sites-enabled/default
+
+# Configure Error 404
+echo "Ceci n'est pas une page" > /var/www/html/404.html
+
+# Restart nginx services 
+sudo service nginx restart
