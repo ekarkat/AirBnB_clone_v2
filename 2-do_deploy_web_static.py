31bbb03f7fcccb0f51e97765212665bf3d986ef7
ekarkat <ekarkat@gmail.com>
2023-12-11 00:56:33 +0000

    ta2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 # Fabric do_deploy
-from fabric.api import put, env, run
+from fabric.api import put, env, run, task
+import re
 
 env.hosts = ["35.153.93.177", "34.203.77.10"]
 
+
+@task
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
     try:
-        file = file = archive_path.split("/")[-1].split(".")[0]
+        file = file = re.search(r'web_static_[0-9]*\.tgz', archive_path)
     except Exception:
         return False
     try:
@@ -19,7 +22,7 @@ def do_deploy(archive_path):
         con.run("rm -rf /data/web_static/current")
         con.run("mv {}/{}/web_static/* {}/{}/".format(pat, file, pat, file))
         con.run("rm -rf {}/{}/web_static/".format(pat, file))
-        con.run("ln -sf {}/{} /data/web_static/current".format(pat, file))
+        con.run("ln -s {}/{} /data/web_static/current".format(pat, file))
         return True
     except Exception:
         return False
