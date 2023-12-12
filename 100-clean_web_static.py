42aae4b878ddc1809d2fff48407f6c487cfc71f6
ekarkat <ekarkat@gmail.com>
2023-12-12 23:29:10 +0000

    ta110

---
+++ 100-clean_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Compress a directory ro .tgz'''
+
+from datetime import datetime
+from fabric.api import local
+from fabric.api import put, env, run, task
+import re
+
+env.hosts = ["35.153.93.177", "34.203.77.10"]
+
+
+def do_clean(number=0):
+    """Clean up old versions"""
+    if number == 0:
+        number = 1
+    # locally:
+    files = local("ls versions | grep web_static_ | sort -r")
+    files_list = files.stdout.split("\n")
+    while len(files_list) > number:
+        if files_list[-1] == "":
+            files_list.pop()
+        local("rm versions/{}".format(files_list.pop()))
+    # remotely:
+    pat = "/data/web_static/releases"
+    files = run("ls versions | grep web_static_ | sort -r")
+    files_list = files.split("\n")
+    while len(files_list) > number:
+        if files_list[-1] == "":
+            files_list.pop()
+        run("rm {}/{}".format(pat, files_list.pop()))
