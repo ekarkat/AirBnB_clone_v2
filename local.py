1504f1d4679b2318131568c82ab89dfe3e40be75
ekarkat <ekarkat@gmail.com>
2023-12-21 18:25:48 +0000

    remove 100-file

--- local.py
+++ local.py
@@ -5,39 +5,17 @@ from datetime import datetime
 # Example local("ls")
 
 
-# date = datetime.utcnow()
-# file = "versions/web_static_{}{}{}{}{}{}".format(date.year,
-# 													date.month,
-# 													date.day,
-# 													date.hour,
-# 													date.minute,
-# 													date.second)
+date = datetime.utcnow()
+file = "versions/web_static_{}{}{}{}{}{}".format(date.year,
+													date.month,
+													date.day,
+													date.hour,
+													date.minute,
+													date.second)
 
-# local("mkdir -p versions")
-# try:
-# 	local("tar -czf {}.tgz web_static".format(file))
-# 	print(file)
-# except Exception:
-# 	print("Error")
-
-name_pattern = "web_static_"
-
-files = local("ls versions | grep web_static_ | sort -tr")
-
-print ("----")
-print((files.stdout))
-
-fool = files.stdout.split("\n")
-
-print(fool)
-
-print("-----------")
-
-
-n = 3
-
-for i in range(n):
-	# if fool[-1] == "":
-	# 	fool.pop()
-	# local("rm versions/{}".format(fool.pop()))
-	print(i)
+local("mkdir -p versions")
+try:
+	local("tar -czf {}.tgz web_static".format(file))
+	print(file)
+except Exception:
+	print("Error")
