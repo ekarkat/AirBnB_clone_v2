4c0f2b7cc47f2ecf441c8d23fcda8aba2cad74e7
ekarkat <ekarkat@gmail.com>
2023-12-24 18:20:44 +0000

    tas4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,8 +24,8 @@ def c_text(text):
     return ("C {}".format(text.replace('_', ' ')))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     # Replace underscores with spaces
     return ("Python {}".format(text.replace('_', ' ')))
