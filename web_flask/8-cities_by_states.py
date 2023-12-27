7e39038bf3ee568cb28410ea4927c3f4d4d08af5
ekarkat <ekarkat@gmail.com>
2023-12-27 01:22:05 +0000

    tas9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,7 +19,7 @@ def cities_by_states():
     # Return state and cities
     result_s = storage.all(State)
     sorted_s = dict(sorted(result_s.items(), key=lambda item: item[1].name))
-    return render_template("8-cities_by_states.html", states=result_s)
+    return render_template("8-cities_by_states.html", states=sorted_s)
 
 
 if __name__ == '__main__':
