b1c8d85496e0cf8dfbf86dfc6578c88783403189
ekarkat <ekarkat@gmail.com>
2023-12-27 00:18:10 +0000

    task8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -24,11 +24,11 @@ class DBStorage():
     def __init__(self):
         """Init method"""
 
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
-                                            getenv("HBNB_MYSQL_USER"),
-                                            getenv("HBNB_MYSQL_PWD"),
-                                            getenv("HBNB_MYSQL_HOST"),
-                                            getenv("HBNB_MYSQL_DB")),
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
